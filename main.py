6dc19a259ff3ab7f9e88963ac271f8ddb1dfe781
Mawv1 <114910699+Mawv1@users.noreply.github.com>
2024-03-19 23:34:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import mediapipe as mp
 import cv2
 import pyautogui
 import win32api
+from concurrent.futures import ThreadPoolExecutor
 
-
-def function(points):
+def function(hand, points):
     normalized_landmark = hand.landmark[points]
     x, y = int(normalized_landmark.x * image_width), int(normalized_landmark.y * image_height)
 
     if points == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-        cv2.circle(image, (x, y), 25, (0, 200, 0), 5)
+        cv2.rectangle(image, (x-12, y-12), (x+12, y+12), (0, 200, 0), 5)
         win32api.SetCursorPos((x * 4, y * 5))
         pyautogui.mouseDown(button='left')
 
@@ -28,8 +28,8 @@ if __name__ == "__main__":
 
     while True:
         ret, frame = video.read()
-        # if not ret:
-        #     break
+        if not ret:
+            break
 
         image = cv2.flip(frame, 1)
         image_height, image_width, _ = image.shape
@@ -40,17 +40,10 @@ if __name__ == "__main__":
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
                                           mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
 
-                lista = list(map(lambda x: function(x), mp_hands.HandLandmark))
-                # for points in mp_hands.HandLandmark:
-                #     normalized_landmark = hand.landmark[points]
-                #     x, y = int(normalized_landmark.x * image_width), int(normalized_landmark.y * image_height)
-                #
-                #     if points == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-                #         cv2.circle(image, (x, y), 25, (0, 200, 0), 5)
-                #         win32api.SetCursorPos((x * 4, y * 5))
-                #         pyautogui.mouseDown(button='left')
+                with ThreadPoolExecutor() as executor:
+                    executor.map(lambda x: function(hand, x), [mp_hands.HandLandmark.INDEX_FINGER_TIP])
 
-        cv2.imshow('game', image)
+        cv2.imshow('Dzien WEEIA', image)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
