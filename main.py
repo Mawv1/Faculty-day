2f96f2532f5c68b73e0ed147dd20d268d899f43b
Mawv1 <114910699+Mawv1@users.noreply.github.com>
2024-03-25 23:34:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import mediapipe as mp
 import cv2
 import pyautogui
 import win32api
-from concurrent.futures import ThreadPoolExecutor
 
 def function(hand, points):
     normalized_landmark = hand.landmark[points]
     x, y = int(normalized_landmark.x * image_width), int(normalized_landmark.y * image_height)
 
     if points == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-        cv2.rectangle(image, (x-12, y-12), (x+12, y+12), (0, 200, 0), 5)
+        cv2.rectangle(image, (x - 12, y - 12), (x + 12, y + 12), (0, 200, 0), 5)
         win32api.SetCursorPos((x * 4, y * 5))
         pyautogui.mouseDown(button='left')
 
@@ -36,12 +35,8 @@ if __name__ == "__main__":
         results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
         if results.multi_hand_landmarks:
-            for hand in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
-
-                with ThreadPoolExecutor() as executor:
-                    executor.map(lambda x: function(hand, x), [mp_hands.HandLandmark.INDEX_FINGER_TIP])
+            hands_to_process = results.multi_hand_landmarks
+            [function(hand, mp_hands.HandLandmark.INDEX_FINGER_TIP) for hand in hands_to_process]
 
         cv2.imshow('Dzien WEEIA', image)
         if cv2.waitKey(10) & 0xFF == ord('q'):
